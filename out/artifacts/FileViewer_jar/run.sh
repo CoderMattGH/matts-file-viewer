d53275e999014b45031b23e503ad826ff174528a
GANDHIX <mattdixon2000@hotmail.com>
2024-01-08 23:26:33 +0000

    Added garbage collection button.

--- out/artifacts/FileViewer_jar/run.sh
+++ out/artifacts/FileViewer_jar/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo RUNNING FileViewer.jar...
+
+# Sets the initial heap size to 16gigabytes and the max to 20gigabytes
+# java -Xms16G -Xmx20G -jar FileViewer.jar
 java -jar FileViewer.jar
