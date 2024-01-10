cfd5bfbe2223a825ba2b749b9251a5af8542145d
GANDHIX <mattdixon2000@hotmail.com>
2024-01-10 11:36:01 +0000

    Added logger

--- out/artifacts/FileViewer_jar/run.sh
+++ out/artifacts/FileViewer_jar/run.sh
@@ -3,4 +3,4 @@ echo RUNNING FileViewer.jar...
 
 # Sets the initial heap size to 16gigabytes and the max to 20gigabytes
 # java -Xms16G -Xmx20G -jar FileViewer.jar
-java -jar FileViewer.jar
+java -jar FILE_VIEWER.jar
