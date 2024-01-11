e1539b495979755c589ae464098be02ed927bf8b
GANDHIX <mattdixon2000@hotmail.com>
2024-01-11 15:23:59 +0000

    Made controller thread safe and updated tests.

--- out/artifacts/FileViewer_jar/run.sh
+++ out/artifacts/FileViewer_jar/run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo RUNNING FileViewer.jar...
 
-# Sets the initial heap size to 16gigabytes and the max to 20gigabytes
-# java -Xms16G -Xmx20G -jar FileViewer.jar
 java -jar FILE_VIEWER.jar
